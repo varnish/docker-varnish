3121f4e974de7d64ae8094c50e99a9906e39ff0c
Guillaume Quintard <guillaume.quintard@gmail.com>
2022-02-13 22:20:10 -0800

    [alpine][next] add vmod-dynamic and varnish-modules

--- populate.sh
+++ populate.sh
@@ -31,28 +31,39 @@ CONFIG='
 		"version": "7.0.2",
 		"tags": "7.0 latest",
 		"pkg-commit": "d3e6a3fad7d4c2ac781ada92dcc246e7eef9d129",
-		"dist-sha512": "5eb08345c95152639266b7ad241185188477f8fd04e88e4dfda1579719a1a413790a0616f25d70994f6d3b8f7640ea80926ece7c547555dad856fd9f6960c9a3"
+		"dist-sha512": "5eb08345c95152639266b7ad241185188477f8fd04e88e4dfda1579719a1a413790a0616f25d70994f6d3b8f7640ea80926ece7c547555dad856fd9f6960c9a3",
+		"varnish-modules-version": "0.19.0",
+		"varnish-modules-sha512sum": "fc6f4c1695f80fa3b267c13c772dca9cf577eed38c733207cf0f8e01b5d4ebabbe43e936974ba70338a663a45624254759cfd75f8fbae0202361238ee5f15cef",
+		"vmod-dynamic-version": "2.5.0",
+		"vmod-dynamic-commit": "4d0ca5230d563d9c0e03df0ec6e01f7c174fdfd5",
+		"vmod-dynamic-sha512sum": "ea9dceb88fb472faaec5e7ff79f65afdcdbfde9661fb460c629bffdcea4a9f51e3499aab9e5c202d382d3460912f502145af21e54a5e4a8ae25b34051a484b35"
 	}
 }'
 
-update_dockerfiles() {
-	DEBIAN=`echo $CONFIG | jq -r ".[\"$1\"][\"debian\"]"`
-	VARNISH_VERSION=`echo $CONFIG | jq -r ".[\"$1\"][\"version\"]"`
-	DIST_SHA512=`echo $CONFIG | jq -r ".[\"$1\"][\"dist-sha512\"]"`
-	PKG_COMMIT=`echo $CONFIG | jq -r ".[\"$1\"][\"pkg-commit\"]"`
+resolve_json() {
+	echo $CONFIG | jq -r ".[\"$1\"][\"$2\"]"
+}
 
+update_dockerfiles() {
 	sed $1/$2/Dockerfile.tmpl \
-		-e "s/@DEBIAN@/$DEBIAN/" \
-		-e "s/@VARNISH_VERSION@/$VARNISH_VERSION/" \
-		-e "s/@DIST_SHA512@/$DIST_SHA512/" \
-		-e "s/@PKG_COMMIT@/$PKG_COMMIT/" \
+		-e "s/@DEBIAN@/$(resolve_json "$1" debian)/" \
+		-e "s/@VARNISH_VERSION@/$(resolve_json "$1" version)/" \
+		-e "s/@DIST_SHA512@/$(resolve_json "$1" dist-sha512)/" \
+		-e "s/@PKG_COMMIT@/$(resolve_json "$1" pkg-commit)/" \
+		-e "s/@VARNISH_MODULES_VERSION@/$(resolve_json "$1" varnish-modules-version)/" \
+		-e "s/@VARNISH_MODULES_SHA512SUM@/$(resolve_json "$1" varnish-modules-sha512sum)/" \
+		-e "s/@VMOD_DYNAMIC_VERSION@/$(resolve_json "$1" vmod-dynamic-version)/" \
+		-e "s/@VMOD_DYNAMIC_COMMIT@/$(resolve_json "$1" vmod-dynamic-commit)/" \
+		-e "s/@VMOD_DYNAMIC_SHA512SUM@/$(resolve_json "$1" vmod-dynamic-sha512sum)/" \
 		> $1/$2/Dockerfile
 }
 
 populate_dockerfiles() {
 	for i in `echo $CONFIG | jq -r 'keys | .[]'`; do
 		update_dockerfiles $i debian
-		[ "$i" != "stable" ] && update_dockerfiles $i alpine
+		if [ "$i" != "stable" ]; then
+			update_dockerfiles $i alpine
+		fi
 	done
 }
 
