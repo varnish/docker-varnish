9e3880d9b6651d360f9eacb1e9456db141c38d67
Guillaume Quintard <guillaume.quintard@varnish-software.com>
2025-10-10 13:11:53 -0700

    build Enterprise images

--- enterprise/debian/build.sh
+++ enterprise/debian/build.sh
@@ -3,9 +3,12 @@ set -ex
 
 DEBIAN_VERSION=bookworm
 IMAGE_VERSION=6.0.16r4
-TAGS="latest 6 6.0 6.0.16 6.0.16r4"
+TAGS="6 6.0 6.0.16 6.0.16r4"
 VARNISH_PLUS_VERSION="$IMAGE_VERSION-1~$DEBIAN_VERSION"
 VARNISH_OTEL_VERSION="1.2.1-1~$DEBIAN_VERSION"
 
-docker build . $(echo "$TAGS" | sed -E 's@(^|\s)+@ -t varnish/enterprise:@g')
-docker push    $(echo "$TAGS" | sed -E 's@(^|\s)+@    varnish/enterprise:@g')
+docker build . -t varnish/enterprise:latest
+for t in $TAGS; do
+	docker tag varnish/enterprise:latest varnish/enterprise:$t
+done
+docker push varnish/enterprise:latest --all-tags
